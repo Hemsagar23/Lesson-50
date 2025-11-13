f0a6352b5fd89bd6bf87445364cf55cac866f82c
Hemsagar23 <kolluhemsagar@gmail.com>
2025-11-13 22:01:23 +0530

    initial commit

---
+++ a2.py
@@ -0,0 +1,24 @@
+file = open('Codingal.txt', 'r')
+print("reading first line.......")
+print(file.readline())
+file.close()
+
+file = open('Codingal.txt', 'r')
+print("Reading multiple lines......")
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+file.close()
+
+file = open('Codingal.txt', 'r')
+print("Looping through the lines......")
+for line in file:
+    print(line)
+file.close()
+
+file = open('Codingal.txt', 'r')
+print("Looping through the lines......")
+for f in file.readlines():
+    print(f)
+file.close()
